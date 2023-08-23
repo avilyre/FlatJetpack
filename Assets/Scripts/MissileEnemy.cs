e6c79e2889a2aef526828377952d9ac3c1445b26
Avily Silva <avilyrs@gmail.com>
2023-08-23 18:45:28 -0300

    :feat: spawn enemies with missile enemy

---
+++ Assets/Scripts/MissileEnemy.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileEnemy : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+    public float moveSpeed = 3f;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        Destroy(gameObject, 3f);
+    }
+
+    void Update()
+    {
+        rigidBody.velocity = Vector3.left * moveSpeed;
+    }
+}
---
+++ Assets/Scripts/SpawnEnemies.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemies : MonoBehaviour
+{
+    private float gameTime = 0;
+    public float spawnTime = 5f;
+
+    public List<GameObject> enemies = new List<GameObject>();
+
+    private void Update()
+    {
+        gameTime += Time.deltaTime;
+
+        if (gameTime >= spawnTime)
+        {
+            SpawnEnemy();
+            gameTime = 0;
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        Instantiate(enemies[0], transform.position + new Vector3(0f, Random.Range(0f, 3f), 0f), transform.rotation);
+    }
+}
