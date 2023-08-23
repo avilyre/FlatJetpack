15fe8e3d679e644af2d57a17726b94e3b3455f69
Avily Silva <avilyrs@gmail.com>
2023-08-23 14:47:09 -0300

    :feat: player animations and collision detector

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,12 +6,16 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rigidBody;
+    private Animator animator;
+
     public float moveSpeed = 3f;
     public float jumpFoce = 10f;
+    private bool isJumping = false;
 
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
@@ -20,11 +24,22 @@ public class Player : MonoBehaviour
         AutoMovement();
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            animator.SetBool("Jumping", false);
+            isJumping = false;
+        }
+    }
+
     void Control()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
         {
             rigidBody.AddForce(new Vector2(0f, jumpFoce), ForceMode2D.Impulse);
+            animator.SetBool("Jumping", true);
+            isJumping = true;
         }
     }
 
